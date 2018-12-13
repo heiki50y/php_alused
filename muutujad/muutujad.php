5c340d8245f248400ec433d9a8207addb12716f7
heiki.kotsar <heiki.kotsar@khk.ee>
2018-12-13 15:28:25 +0200

    muutujate kasutamine

---
+++ muutujad/muutujad.php
@@ -0,0 +1,16 @@
+<?php
+// muutujate defineerimine
+// $muutujaNimi = väärtus;
+//
+
+$taisArv = 7;
+$reaalArv = 3.14;
+$sone = 'kodu';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+
+echo 'Täisarv = '.$taisArv.'<br>';
+echo 'Reaalarv = '.$reaalArv.'<br>';
+echo 'Sõne = '.$sone.'<br>';
+echo 'True = '.$toevaartusTrue.'<br>';
+echo 'False = '.$toevaartusFalse.'<br>';
