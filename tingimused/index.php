59890d6b670c6c1b392e22afccc355e3ba08a002
heiki.kotsar <heiki.kotsar@khk.ee>
2018-12-13 16:12:14 +0200

    Tingimused

---
+++ tingimused/index.php
@@ -0,0 +1,22 @@
+<?php
+// tingimused
+// if tingimus
+// tegevused mis toimuvad kui tingimused kehtivad
+// else
+// tegevused, mis toimuvad kui tingimus ei kehti
+
+echo '<h4>Tingimused</h4>';
+$arv = rand(0,100);
+$varv = '';
+
+if ($arv > 0 and $arv <25){
+    $varv = 'green';
+
+} else if ($arv > 25 and $arv < 50) {
+    $varv = 'blue';
+} else if ($arv >= 50 and $arv < 75) {
+    $varv = 'red';
+} else if ($arv >= 75 and $arv < 101) {
+    $varv = 'pink';
+}
+echo '<p style="color: '.$varv.' ">'.$arv.'</p>';
