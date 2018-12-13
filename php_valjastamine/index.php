3f05e428bfaa554575c8228edf0b6b3d1ae178c2
heiki.kotsar <heiki.kotsar@khk.ee>
2018-12-13 15:04:42 +0200

    väljastus commit echo

---
+++ php_valjastamine/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heiki.kotsar
+ * Date: 13.12.2018
+ * Time: 14:49
+ */
+
+print 'See on esimene lause php keeles'.'<br>';
+print 'See on teine lause php keeles'. '<br>';
+// testi valjastus echo kasuga
+echo 'See tekst on echo abil valjastatud'. ;
+
