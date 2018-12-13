de2f4fb3b3248ecd567bbbbe6cb7ef52fdd63ae3
heiki.kotsar <heiki.kotsar@khk.ee>
2018-12-13 15:09:41 +0200

    väljastus HTML-lis

--- php_valjastamine/index.php
+++ php_valjastamine/index.php
@@ -1,13 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <title>php alused - valjastus</title>
+    </head>
+    <body>
+    <h4>Väljastus</h4>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: heiki.kotsar
- * Date: 13.12.2018
- * Time: 14:49
- */
-
-print 'See on esimene lause php keeles'.'<br>';
-print 'See on teine lause php keeles'. '<br>';
-// testi valjastus echo kasuga
-echo 'See tekst on echo abil valjastatud'. ;
-
+echo 'See tekst on echo abil valjastatud'. '<br>' ;
+?>
+    </body>
+</html>
