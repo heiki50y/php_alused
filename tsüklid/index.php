718fbc37569f16f225f0cacfadcb4be20e6dfd64
heiki.kotsar <heiki.kotsar@khk.ee>
2018-12-14 11:33:32 +0200

    Tsüklid: while

--- tsüklid/index.php
+++ tsüklid/index.php
@@ -17,10 +17,11 @@ for($arv = 1; $arv <= 10; $arv++) {
     echo '<div style="color: '.$varv.'">'.$varv.'</div>';
 }
 */
+/*
 echo '<table>';
-for($rida = 1; $rida <= 5; $rida++){
+for($arv = 1; $arv <= 5; $arv++){
     echo '<tr>';
-    for($veerg = 1; $veerg <= 5; $veerg++) {
+    for($arv2 = 1; $arv2 <= 5; $arv2++) {
         $varv = '#';
         for($kord = 1; $kord <= 6; $kord++){
             $varv = $varv.dechex(rand(0,15));
@@ -32,3 +33,73 @@ for($rida = 1; $rida <= 5; $rida++){
     echo '</tr>';
 }
 echo '</table>';
+*/
+
+echo '</table>';
+echo '<hr>';
+// korrutustabel
+echo '<table style="border-collapse: collapse">';
+// ülemine punane riba
+for($arv2 = 0; $arv2 <= 10; $arv2++){
+    /* if($arv2 == 0){
+         $txt = '&nbsp;';
+     } else {
+         $txt = $arv2;
+     }*/
+    $txt = ($arv2 == 0) ? '&nbsp;' : $arv2;
+    echo '<th style="width: 20px; text-align: center; border: solid 1px black; background: lightgreen">';
+    echo $txt;
+    echo '</th>';
+}
+// põhitabel
+for($arv1 = 1; $arv1 <= 10; $arv1++){
+    echo '<tr>'; // rea algus
+    // üks punane kast
+    echo '<th style="width: 20px; text-align: center; border: solid 1px black; background: lightcoral">';
+    echo $arv1;
+    echo '</th>';
+    // korrutustabeli elemendid - korrutamise tulemused
+    for($arv2 = 1; $arv2 <= 10; $arv2++) {
+        echo '<td style="width: 20px; text-align: center; border: solid 1px black;">';
+        echo $arv1 * $arv2;
+        echo '</td>';
+    }
+    echo '</tr>'; // rea lõpp
+}
+echo '</table>'; // tabeli lõpp
+
+echo '<hr>';
+
+$arv  = 123;
+$numbriteSumma = 0;
+while ($arv != 0) {
+    $number = $arv % 10;
+    echo 'Number = '.$number.'<br>';
+    $arv = $arv / 10;
+    settype($arv, 'int');
+    echo 'Uus arv on '.$arv. '<br>';
+    $numbriteSumma = $numbriteSumma + $number;
+    echo 'Hetkel summa = '.$numbriteSumma. '<br>';
+}
+echo '<b>Lõplik summa = '.$numbriteSumma.'<br></br>';
+
+echo '<hr>';
+
+$arv = 442158755745;
+// leia mitu korda siin esinemas 5;
+$numberkokku = 0;
+while ($arv !=0) {
+    $number = $arv % 10;
+    if ($number == 5) {
+        $numberkokku = $numberkokku + 1;
+    }
+    $arv = $arv / 10;
+    settype($arv, 'int');
+}
+echo 'Number 5 esineb antud arvus '.$numberkokku.' korda<br>';
+
+
+
+
+
+
