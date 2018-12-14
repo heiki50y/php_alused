5d61386b2398cb7326eaf21df456a36c3efb072c
heiki.kotsar <heiki.kotsar@khk.ee>
2018-12-14 11:41:17 +0200

    Funktsioon

--- funktsioonid/index.php
+++ funktsioonid/index.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: heiki.kotsar
- * Date: 14.12.2018
- * Time: 11:31
- */
+
+function numbriKordumine($arv, $kontrollNumber) {
+
+    echo 'Arv on ' . $arv . ' <br>';
+    $numberkokku = 0;
+    while ($arv !=0) {
+        $number = $arv % 10;
+        if ($number == $kontrollNumber) {
+            $numberkokku = $numberkokku + 1;
+        }
+        $arv = $arv / 10;
+        settype($arv, 'int');
+    }
+    echo 'Number '.$kontrollNumber.'esineb antud arvus ' . $numberkokku . 'korda<br>';
+}
+
+numbriKordumine(442158755745,  5);
+
+
+
+
+
