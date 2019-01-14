f8325b661e5028550f3771e798d2e4f4613059dc
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-14 10:58:16 +0200

    Massiivi elementide loomine identifikaatori abil

--- Arrays/arrays.php
+++ Arrays/arrays.php
@@ -1,4 +1,15 @@
 <?php
-
+/*
 $users = array("Bret", "Sharon", "Betty", "Harry");
 print "$users[2]";
+*/
+
+$users[ ] = "Bret";
+$users[ ] = "Sharon";
+$users[ ] = "Betty";
+$users[ ] = "Harry";
+
+$users = array("Bret", "Sharon", "Betty", "Harry" );
+$users[] = "Sally";
+
+
