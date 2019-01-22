4eaa8602a0bd91d3bfb5935b2b7251a99cb98ac3
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-22 10:59:45 +0200

    strtolower() – muudab teksti väiketähtedeks strtoupper() – muudab teksti SUURTÄHTEDEKS

---
+++ text/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heiki.kotsar
+ * Date: 22.01.2019
+ * Time: 10:54
+ */
+
+$tekst = 'Life is about ingnoring the drama.';
+
+    echo strtolower($tekst);
+    echo '<br>';
+    echo strtoupper($tekst);
