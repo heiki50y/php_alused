f3df479ee64dd6b560c1057e40d932c647befd75
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-22 11:27:02 +0200

    Tekst kui massiiv: PHP käsitleb teksti kui massiivi, kus esimene märk on indeksiga 0 jne. Funktsiooni substr(), kus esimene arv määrab mitmenast indeksist alustatakse ning teine mitmendast lõpetatakse.

--- text/index.php
+++ text/index.php
@@ -38,3 +38,23 @@ echo strip_tags($tekst);
 echo '<br>';
 $tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
 echo strip_tags($tekst, '<b>, <br>');
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+echo $tekst[0]; 				//A
+echo '<br>';
+echo $tekst[4];
+
+echo '<br>';
+echo '<br>';
+
+$tekst = 'All thinking men are atheists';
+echo substr($tekst, 3,5);		//thin
+echo '<br>';
+echo substr($tekst, 4, -13);	//thinking men
+echo '<br>';
+echo substr($tekst, -8, 7);
