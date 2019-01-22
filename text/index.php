cefe570b63f295e7c358973b082b2530b4fe8ab6
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-22 11:22:17 +0200

    Teksti kärpimine

--- text/index.php
+++ text/index.php
@@ -18,7 +18,23 @@ $tekst = 'Life is about ingnoring the drama.';
     echo '<br>';
 
 
-$tekst = 'Experience is the teacher of fools';
+$tekst = 'Life is about ingnoring the drama.';
 echo strlen($tekst);
 echo '<br>';
 echo str_word_count($tekst);
+echo '<br>';
+
+$tekst = '  Life is about ingnoring the drama.   ';
+    echo "<pre> $tekst </pre>";
+    echo "<pre>".trim($tekst)."</pre>";
+    echo "<pre>".ltrim($tekst)."</pre>";
+    echo "<pre>".rtrim($tekst)."</pre>";
+echo '<br>';
+$tekst = 'A woman should soften but not weaken a man';
+echo trim($tekst, "A, a, k..n, w");
+echo '<br>';
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst);
+echo '<br>';
+$tekst = '<b>Experience</b> <a href="#">is</a> the teacher <br>of fools';
+echo strip_tags($tekst, '<b>, <br>');
