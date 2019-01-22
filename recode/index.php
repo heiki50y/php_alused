f7ec9bb5a4da7605913f6611b6b79953860e1ff7
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-22 12:28:56 +0200

    Avame index.php ja loome lingid, mille kaudu saab kasutaja öelda, millise faili sisu vaja on (jätsin harjutusest päise ja jaluse välja).

---
+++ recode/index.php
@@ -0,0 +1,8 @@
+
+<menu>
+    <a href="index.php">Avaleht</a>
+    <a href="index.php?leht=portfolio">Portfoolio</a>
+    <a href="index.php"leht="kaart">Kaart</a>
+    <a href="index.php">Kontakt</a>
+</menu>
+
