6db3933ed3a84a74209e1467acfe7cec82b0b608
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-28 14:09:47 +0200

    function htmlFromFile

--- db/fnk.php
+++ db/fnk.php
@@ -5,3 +5,11 @@
  * Date: 28.01.2019
  * Time: 14:07
  */
+
+function htmlFromFile($fileName){
+    $fileName = 'html/'.$fileName.'.html';
+    $fp = fopen($fileName, 'r');
+    $content = fread($fp, filesize($fileName));
+    fclose($fp);
+    return $content;
+}
