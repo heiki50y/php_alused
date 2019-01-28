5de66c7e9be247db671c0f7610bc5ad14abe2870
heiki.kotsar <heiki.kotsar@khk.ee>
2019-01-28 14:20:42 +0200

    login.php

---
+++ db/login.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heiki.kotsar
+ * Date: 28.01.2019
+ * Time: 14:19
+ */
+
+$username = $_GET['uname'];
+$password = $_GET['upass'];
+echo $username.' '.$password;
